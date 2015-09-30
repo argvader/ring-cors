09176546acdfeea9721ec9886e50b2ea85e899fa
Gary Paige <gary@theoryblister.com>
2015-09-30 11:45:38 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.3.2"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-core "1.4.0"]]
   :deploy-repositories [["releases" :clojars]]
   :plugins [[jonase/eastwood "0.2.1"]
-            [lein-bikeshed "0.1.6"]
+            [lein-bikeshed "0.2.0"]
             [lein-difftest "2.0.0"]]
   :eastwood {:exclude-linters [:bad-arglists]}
   :aliases {"ci" ["do" ["with-profile" "dev:dev,1.4:dev,1.5:dev" "difftest"] ["lint"]]
